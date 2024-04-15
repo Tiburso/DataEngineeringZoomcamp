b931be375b4400082ae3627d7f2fc51c71a113c0
Tiburso <manuelmariamc@gmail.com>
2024-04-15 16:50:14 +0200

    Add categorical data view to dbt

---
+++ dbt/models/staging/categorical_data_view.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='view') }}
+
+-- D1H rainfall duration in the last hour
+-- Select the count of stations where it has rained for more than 1 hour in the day
+
+SELECT 
+    DISTINCT stationname, COUNT(stationname) AS count_stationname
+
+FROM 
+    {{ source('staging', 'weather_data') }}
+WHERE
+    D1H >= 30
+GROUP BY 
+    stationname
+    
+{% if var('is_test_run', default=true) %}
+    LIMIT 100
+{% endif %}
